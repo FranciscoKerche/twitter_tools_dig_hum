09aeaa0367691322755d662b7f206bfc68ef8632
FranciscoKerche <74992290+FranciscoKerche@users.noreply.github.com>
2021-03-30 10:10:38 -0300

    Update Funcao_unir_datasets.R

--- Funcao_unir_datasets.R
+++ Funcao_unir_datasets.R
@@ -1,13 +1,16 @@
 pacman::p_load(rio, tidyverse)
 
+#A proposta da função é unir diversos data-sets que sejam extraídos em um monitoramento diário ou mensal
+
 imp_neps <- function(termo){
-  a <- list.files(str_c("Teste_tweets/", termo), recursive = T, full.names = T) %>%
+  a <- list.files(str_c("Teste_tweets/", termo), recursive = T, full.names = T) %>% #O nome "Teste_tweets/" pode ser trocado para qualquer nome de pasta
     sapply(import, simplify = F) %>%
     bind_rows() %>%
-    tibble()
-  b <-  a[order(a[,'status_id'],-a[,'retweet_followers_count']),]
+    tibble() #A proposta é que se o comentador foi pego duas vezes, encontrar algum perfil que apareça duas vezes seguida com o mesmo tweet
+  #Seja possível eliminar todas as duplicatas
+  b <-  a[order(a[,'status_id'],-a[,'retweet_followers_count']),] #Pega apenas o mais recente número de followers
   t <-  b[!duplicated(a$status_id),]
-  c <-  t[order(t[,'status_id'],-t[,'favourites_count']),]
+  c <-  t[order(t[,'status_id'],-t[,'favourites_count']),] #Pega apenas o mais recente número de favorites
   d <-  c[!duplicated(c$status_id),]
-  d %>% arrange(screen_name)
+  d %>% arrange(screen_name) #No final, unificar o data-set pronto
 }
